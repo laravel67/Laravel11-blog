65fe9a8debf9fa4c8cfa8c8eb6e9930f1f774fbc
laravel developer <laravel1010@gmail.com>
2024-05-23 11:46:26 +0700

    Push From Windows

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite('resources/css/app.css')
+    <title>Larave Blog</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     {{-- <title>Laravel 11 | {{ $ }}</title> --}}
 </head>
